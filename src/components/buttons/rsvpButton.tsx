5296ea12771d8129c064ff54313dae015df50e25
Dalyn Dunn <djld30@gmail.com>
2023-07-03 23:08:11 -0400

    slight button update

--- src/components/buttons/rsvpButton.tsx
+++ src/components/buttons/rsvpButton.tsx
@@ -5,6 +5,6 @@ import React from 'react'
 export default function RSVPButton({toggleRSVP}: {toggleRSVP: any}) {
   
   return (
-    <div className="border-2 border-pink-300 py-2 px-4 hover:bg-pink-300 text-pink-300 hover:text-neutral-50 text-lg transition linear duration-300 cursor-pointer" onClick={toggleRSVP}>RSVP</div>
+    <div className="border-2 border-pink-300 py-2 px-4 hover:bg-pink-300 text-pink-300 hover:text-neutral-50 text-lg md:text-2xl transition linear duration-300 cursor-pointer" onClick={toggleRSVP}>RSVP</div>
   )
 }
