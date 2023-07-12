a74d1455d039bc868730baae96ba02f7e6b006cb
Dalyn Dunn <djld30@gmail.com>
2023-07-12 15:22:32 -0400

    final commit, all info and ui complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     <div className="h-screen relative flex justify-center items-center bg-butterfly">
       {showRSVP && <RSVP setShowRSVP={setShowRSVP} setShowRegistry={setShowRegistry} />}
       {showRegistry && <Registry setShowRSVP={setShowRSVP} setShowRegistry={setShowRegistry} />}
-      <div className="h-full w-full flex flex-col justify-center items-center gap-8 text-center bg-container">
+      <div className="h-full w-full flex flex-col justify-center items-center gap-6 text-center bg-container">
         <div className="flex flex-col justify-center items-center gap-4">
           <p className="text-purple-400 text-2xl md:text-3xl">A little</p>
           <p className="text-pink-300 text-6xl md:text-8xl font-greatVibes">Butterfly</p>
@@ -33,8 +33,15 @@ export default function Home() {
         </div>
         <p className="text-black text-xl md:text-2xl">Join us for a baby shower in honor of:</p>
         <h1 className="text-pink-300 text-6xl md:text-8xl font-greatVibes">Kensley-Lincoln Alisa Dunn</h1>
-        <p className="text-purple-400 text-lg md:text-2xl">When:</p>
-        <p className="text-black text-lg md:text-2xl">Where:</p>
+        <div>
+          <p className="text-purple-400 text-xl md:text-3xl">When:</p>
+          <p className="text-black text-md md:text-xl">Saturday, September 30<sup>th</sup>, 2023</p>
+          <p className="text-black text-md md:text-xl">2-5pm</p>
+        </div>
+        <div>
+          <p className="text-purple-400 text-xl md:text-3xl">Where:</p>
+          <p className="text-black text-md md:text-xl">1365 W Orange St, Jesup, GA 31545</p>
+        </div>
         <div className="flex justify-center items-center gap-8">
           <RSVPButton toggleRSVP={toggleRSVP} />
           <RegistryButton toggleRegistry={toggleRegistry} />
